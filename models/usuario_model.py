26c87724a4fb6b81202cf4d07b71e429435c413c
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2021-06-16 08:40:21 -0300

    Format files

--- models/usuario_model.py
+++ models/usuario_model.py
@@ -1,11 +1,11 @@
 class UsuarioModel:
-    
+
     def __init__(self, id, nome, email, senha):
         self.id = id
         self.nome = nome
         self.email = email
         self.senha = senha
-        
+
     def json(self):
         return {
             'id': self.id,
--- resources/usuario.py
+++ resources/usuario.py
@@ -5,72 +5,72 @@ from models.usuario_model import UsuarioModel
 usuarios = [
     {
         'id': 1,
-        'nome':'João Vitor',
+        'nome': 'João Vitor',
         'email': 'jvgsilva180@gmail.com',
         'senha': '123456',
     },
     {
         'id': 2,
-        'nome':'Jaqueline',
+        'nome': 'Jaqueline',
         'email': 'jaqueline@gmail.com',
         'senha': '123456',
     },
     {
         'id': 3,
-        'nome':'Tião',
+        'nome': 'Tião',
         'email': 'tiao@gmail.com',
         'senha': '123456',
     }
 ]
 
+
 class Usuarios(Resource):
-    
+
     def get(self):
         return {'usuarios': usuarios}
-    
-    
+
+
 class Usuario(Resource):
-    
     args = reqparse.RequestParser()
     args.add_argument('nome')
     args.add_argument('email')
     args.add_argument('senha')
-    
+
     def find_usuario(id):
         for usuario in usuarios:
             if usuario['id'] == id:
                 return usuario
         return None
-    
+
     def get(self, id):
         usuario = Usuario.find_usuario(id)
-        if usuario :
+        if usuario:
             return usuario
-        
+
         return {'message': 'Usuario não encontrado'}, 404
-    
+
     def post(self, id):
-        
+
         dados = Usuario.args.parse_args()
         novo_usuario = UsuarioModel(id, **dados)
         novo_json = novo_usuario.json()
-        
+
         usuarios.append(novo_json)
         return novo_json, 200
-    
+
     def put(self, id):
         dados = Usuario.args.parse_args()
         novo_usuario = UsuarioModel(id, **dados)
         novo_json = novo_usuario.json()
-        
+
         usuario = Usuario.find_usuario(id)
-        if usuario :
+        if usuario:
             usuario.update(novo_json)
             return novo_json, 200
-        
+
         usuarios.append(novo_json)
         return novo_json, 201
-    
+
     def delete(self, id):
         global usuarios
         usuarios = [usuario for usuario in usuarios if usuario['id'] != id]
